a78badf162bbb44fafd4da8f749f63e0bee1d681
vindevoghel <yvindevoghel @gmail.com>
2019-08-28 16:53:51 +0200

    there was an attempt 7.5

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -10,7 +10,19 @@
 // You will have time to focus on it later.
 
 (function() {
+    const p1 = document.getElementById("part-1");
+    const p1min = p1.getAttribute("data-min");
+    const p1max = p1.getAttribute("data-max");
 
-    // your code here
+    const p2 = document.getElementById("part-2");
+    const p3 = document.getElementById("part-3");
+    const p4 = document.getElementById("part-4");
+    const othermin = p2.getAttribute("data-min");
+    const othermax = p2.getAttribute("data-max");
+
+
+
+    setInterval(function(){
+        p2.value = Math.floor(Math.random() * (othermax - othermin + 1) ) + othermin; }, 500);
 
 })();
