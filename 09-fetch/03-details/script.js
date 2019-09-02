6f888bd9d4fbf646deae8200936607766fe976f9
vindevoghel <yvindevoghel @gmail.com>
2019-09-02 17:02:27 +0200

    fetch 3 solved

--- 09-fetch/03-details/script.js
+++ 09-fetch/03-details/script.js
@@ -10,19 +10,24 @@
 // You will have time to focus on it later.
 
 (() => {
-    let inputValue;
-    var temp = document.getElementById("tpl-hero");
-    let powers = document.querySelector(".powers");
-    let alterego = document.querySelector(".alter-ego");
-    let name = document.querySelector(".name");
-    let target = document.getElementById("target");
-    let input = document.getElementById("hero-id");
-    document.getElementById("run").addEventListener("click", function (){
+    document.getElementById("run").addEventListener("click", function () {
         fetch("../../_shared/api.json")
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
             })
-            .then(function(data) {
-                var hero
+            .then(function (data) {
+                let temp = document.getElementById("tpl-hero");
+                let name = temp.content.querySelector(".name");
+                let alterEgo = temp.content.querySelector(".alter-ego");
+                let powers = temp.content.querySelector(".powers");
+                let target = document.getElementById("target");
+                i = document.getElementById("hero-id").value;
+
+                name.innerHTML = data.heroes[i].name;
+                alterEgo.innerHTML = data.heroes[i].alterEgo;
+                powers.innerHTML = data.heroes[i].abilities;
+                let clone = temp.content.cloneNode(true);
+                target.appendChild(clone);
             })
+    })
 })();
